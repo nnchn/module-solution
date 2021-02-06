41ac3621fa1e942bceb05a5a480ce9f7c1cc364d
nnchn <66710689+nnchn@users.noreply.github.com>
2021-02-06 09:20:29 +0800

    Update SpeakGoodbye.js

--- mod-4/SpeakGoodbye.js
+++ mod-4/SpeakGoodbye.js
@@ -1,10 +1,10 @@
+
 (function(window) {
-  var byeSpeaker = new Object();
-  var speakWord = "Good Bye";
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
 
-  byeSpeaker.speak = function(name) {
-    console.log(speakWord + "" + name); 
-  };
-
-  window.byeSpeaker = byeSpeaker;
 })(window);
