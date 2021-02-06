bb4de72ab0fe4ba43587e8a2f71dab32a987251c
nnchn <66710689+nnchn@users.noreply.github.com>
2021-02-06 09:12:18 +0800

    Update script.js

--- mod-4/script.js
+++ mod-4/script.js
@@ -5,9 +5,23 @@
     var firstLetter = names[i].charAt(0).toLowerCase();
 
     if (firstLetter === 'j') {
-      byeSpeaker.speak(names[i]);
+      //byeSpeaker.speak(names[i]);
     } else {
       helloSpeaker.speak(names[i]);
+    }
+  }
+})();
+
+(function() {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      //helloSpeaker.speak(names[i]);
     }
   }
 })();
